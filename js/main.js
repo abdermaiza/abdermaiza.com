3a7ab80b7548befcb0316cf0ea43e737a4af2119
abder <abdermaiza@gmail.com>
2017-01-03 20:22:47 +0100

    remove parallax effect 03 jan

--- js/main.js
+++ js/main.js
@@ -76,10 +76,6 @@ jQuery(document).ready(function ($) {
   $(window).scroll(function(){
       var wScroll = $(this).scrollTop();
 
-      $('.fore').css({
-        'transform' : 'translate(0px, '+ wScroll /5 + '%)'
-      });
-
       $('.fleche').css({
         'transform' : 'translate(0px, '+ wScroll /2 + '%)'
       });
